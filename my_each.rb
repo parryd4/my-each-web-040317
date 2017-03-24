dfbf679c6c65bb6e25398dfa8c4e413cea5c210b
Daniel Parry <parryd424@gmail.com>
2017-03-24 10:58:48 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,16 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
+  count = 0
+  cant_grasp_yield_array = []
+  while count < array.length
+    yield(array[count]) #wtf
+    cant_grasp_yield_array << array[count]
+    count += 1
+  end
+  cant_grasp_yield_array
+end
+
+collection = ['hi','hello','bye','goodbye']
+my_each(collection) do |i|
+  i
 end
